df2bf9dc7dfe5064e3850260cd748c418f62e2b8
Thales Fernandes <thalesgarcezf@gmail.com>
2026-05-13 21:24:53 -0300

    Ajustando erros do tratamento de exceções

--- src/main/java/br/com/bytebank/notification/infrastructure/config/RabbitMQConfig.java
+++ src/main/java/br/com/bytebank/notification/infrastructure/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package br.com.bytebank.notification.infrastructure.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -10,6 +12,11 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String TRANSACTION_COMPLETED = "transaction.completed";
+
+    @Bean
+    public Queue transactionCompletedQueue() {
+        return QueueBuilder.durable(TRANSACTION_COMPLETED).build();
+    }
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
